d260a346f004d916550b053dc708537642e0e0a9
neuroleptique <corn.xiao@gmail.com>
2023-03-26 16:07:21 -0400

    remove non existing controller paths

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,8 +1,6 @@
 const express = require("express")
 const router = express.Router()
 const authController = require("../controllers/auth")
-const homeController = require("../controllers/home")
-const accountsController = require("../controllers/accounts")
 const { ensureAuth, ensureGuest } = require("../middleware/auth")
 
 // Main Routes
