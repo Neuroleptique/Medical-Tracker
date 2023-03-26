6a5de01ab4a6940b5bf08d49e19db30408edef2d
neuroleptique <corn.xiao@gmail.com>
2023-03-26 15:56:40 -0400

    add client login signup and logout auth logics

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,28 @@
 import Landing from "./pages/Landing"
-import Login from "./pages/Login"
-import Register from "./pages/Register"
+import Login from "./pages/LoginForm"
+import Register from "./pages/SignupForm"
 import Dashboard from "./pages/Dashboard"
 import Header from "./components/Header"
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
-  <>
+    <>
 
-<Router>
-      <div className="flex justify-center items-center py-8">
-        <Header />
-      </div>  
+      <Router>
+        <div className="flex justify-center items-center py-8">
+          <Header />
+        </div>
 
-      <div className='container'>
-        <Routes>
-          <Route path='/' element={<Dashboard />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-        </Routes>
-      </div>
-    </Router> 
-
-    <main className="flex justify-center items-center">
-      <Landing />
-    </main>
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<Landing />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/dashboard' element={<Dashboard />} />
+          </Routes>
+        </div>
+      </Router>
 
 
     </>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,30 +1,51 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import {FaSignOutAlt} from 'react-icons/fa'
+import {IoMdLogIn} from 'react-icons/io'
+import {TfiWrite} from 'react-icons/tfi'
+import {Link, useNavigate} from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
+import {logout, reset} from '../features/auth/authSlice'
 
 function Header() {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const {user} = useSelector((state) => state.auth)
+
+  const onLogout = () => {
+    dispatch(logout())
+    dispatch(reset())
+    navigate('/')
+  }
 
   return (
-      <header className='header'>
-        <div className="logo">
-          <Link to='/about'></Link>
-        </div>
-        <ul>
-        <>
-          <li> 
-          <Link to='/login'>
-            <p>Login </p>
-          </Link>
-        </li>
-  
+    <header className='header'>
+      <div className="logo">
+        <Link to='/'>LOGO</Link>
+      </div>
+      <ul>
+        {user ? (
+          <li>
+            <button className='btn' onClick={onLogout}>
+              <FaSignOutAlt />Logout
+            </button>
+          </li>
+        ) : (
+      <>
         <li>
-          <Link to='/register'>
-            <p>Register</p>
-          </Link>
-        </li>
-        </>
-        </ul>
-        </header>        
+        <Link to='/login'>
+          <IoMdLogIn />Login
+        </Link>
+      </li>
+
+      <li>
+        <Link to='/register'>
+          <TfiWrite />Sign up
+        </Link>
+      </li>
+      </>
+      )}
+
+      </ul>
+    </header>
   )
 }
 
