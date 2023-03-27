f86c363ea3414095c5b7ad7986be64e2e5f9ffd7
neuroleptique <corn.xiao@gmail.com>
2023-03-26 22:03:19 -0400

    change redirection to dashboard after user signed up or loged in

--- client/src/pages/LoginForm.jsx
+++ client/src/pages/LoginForm.jsx
@@ -27,7 +27,7 @@ function Login() {
       }
 
       if(isSuccess || user) {
-        navigate('/')
+        navigate('/dashboard')
       }
 
       dispatch(reset())
--- client/src/pages/SignupForm.jsx
+++ client/src/pages/SignupForm.jsx
@@ -30,7 +30,7 @@ function Register() {
     }
 
     if(isSuccess || user) {
-      navigate('/')
+      navigate('/dashboard')
     }
 
     dispatch(reset())
